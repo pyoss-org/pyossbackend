9a3f883ba1d133487eb8ba535773cfd72a0b40d8
drareeg_DT <geerard.ponnet@gmail.com>
2018-02-15 14:37:46 +0100

    Cors filter allows all access from localhost 4200

--- src/main/java/pyoss/StubConfiguration.java
+++ src/main/java/pyoss/StubConfiguration.java
@@ -2,14 +2,29 @@ package pyoss;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @org.springframework.context.annotation.Configuration
 @Profile("stub")
+@EnableWebMvc
 public class StubConfiguration {
 
     @Bean
     public TimeSlotRepository timeSlotRepository() {
         return new TimeSlotRepositoryStub();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 
 }
